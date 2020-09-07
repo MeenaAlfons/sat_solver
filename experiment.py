21f8c02ac7c69ce0df5beaf35513fa8caa2c4173
Andreas Sauter <andreas.sauter@student.uni-tuebingen.de>
2020-09-07 13:01:58 +0200

    blueprint experiments

---
+++ experiment.py
@@ -0,0 +1,11 @@
+def main():
+    # load sudoku rules fo experimental constraints
+    # load sudokus we ant to test on
+
+    # for every constraint set run solver on sudoku.
+    #    for each sudoku report the number of splits or other metrics and which constraint set was used
+
+    # save results in '[expnr]_experiment.txt'
+
+if __name__ == "__main__":
+    main()

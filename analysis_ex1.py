6f3e51fd28ef9f527c76b513bc898a26a008d6f3
Andreas Sauter <andreas.sauter@student.uni-tuebingen.de>
2020-09-17 17:36:59 +0200

    post hoc analysis

--- analysis_ex1.py
+++ analysis_ex1.py
@@ -1,10 +1,8 @@
 import pandas
 from pandas import DataFrame
 from matplotlib import pyplot as plt
-from scipy.stats import shapiro, friedmanchisquare
-
-# set true if you want to plot stuff
-plotting = False
+from scipy.stats import shapiro, friedmanchisquare, wilcoxon
+from scikit_posthocs import posthoc_nemenyi_friedman
 
 # load the data
 data = pandas.read_csv('results/solver_comparison.csv')
@@ -20,7 +18,9 @@ flip_data = DataFrame(columns=['DLIS(True)', 'DLIS(False)', 'JW-OS', 'DLCS', 'HE
                       index=range(1, int(len(data)/5)+1))
 backtrack_data = DataFrame(columns=['DLIS(True)', 'DLIS(False)', 'JW-OS', 'DLCS', 'HERO'],
                       index=range(1, int(len(data)/5)+1))
-split_data = {'loop':loop_data, 'flip':flip_data, 'backtrack':backtrack_data}
+unit_data = DataFrame(columns=['DLIS(True)', 'DLIS(False)', 'JW-OS', 'DLCS', 'HERO'],
+                      index=range(1, int(len(data)/5)+1))
+split_data = {'loop':loop_data, 'flip':flip_data, 'backtrack':backtrack_data, 'unit':unit_data}
 
 # fill in the values
 for key, _ in grouped_data:
@@ -29,9 +29,10 @@ for key, _ in grouped_data:
     loop_data[key] = curr_group['loop']
     flip_data[key] = curr_group['flip']
     backtrack_data[key] = curr_group['backtrack']
+    unit_data[key] = curr_group['unit']
 
 # plot boxplots
-if plotting:
+if False:
     for curr_data in split_data:
         df = split_data[curr_data]
         col = df.columns.values
@@ -55,4 +56,23 @@ for curr_data in split_data:
                             split_data[curr_data]['DLIS(False)'],
                             split_data[curr_data]['JW-OS'],
                             split_data[curr_data]['DLCS'],
-                            split_data[curr_data]['HERO'])) #'DLIS(False)', 'JW-OS', 'DLCS', 'HERO']))
+                            split_data[curr_data]['HERO']))
+
+    # pvalue matrix for pairwise test
+    # Indicates that for all metrics HERO and DLIS(True) are not
+    # significantly different. Neither are the other solvers between
+    # them.
+    print('\nPairwise comparison:')
+    print(posthoc_nemenyi_friedman(split_data[curr_data].values))
+
+# Test whether there is a significant difference between HERO and DLIS(True)
+for curr_data in split_data:
+    if False:
+        split_data[curr_data].boxplot(column=['HERO', 'DLIS(True)'])
+        plt.title(curr_data)
+        plt.ylim(0, 40)
+        plt.show()
+    print('\nWilcox test for HERO against DLIS(True) for ' + curr_data + ' counts: ')
+    print(wilcoxon(split_data[curr_data]['HERO'],
+                   split_data[curr_data]['DLIS(True)'],
+                   alternative='less'))

05fe812525073ffb66af02da5685c640ff40156b
Meena Alfons <meena.alfons@messagebird.com>
2020-09-18 15:50:16 +0200

    decisionHeuristicFactory as parameter

--- DiffConstraintsExperiment.py
+++ DiffConstraintsExperiment.py
@@ -26,7 +26,8 @@ __email__ = "meena.kerolos@gmail.com"
 __status__ = "Development"
 
 class DiffConstraintsExperiment:
-    def __init__(self):
+    def __init__(self, decisionHeuristicFactory):
+        self.decisionHeuristicFactory = decisionHeuristicFactory
         self.rulesCreator = SudokuRules(9)
 
     def rulesOf(self, rows, cols, blocks):
@@ -73,8 +74,6 @@ class DiffConstraintsExperiment:
         return rulesDict
 
     def run(self, numOfConstraints, timeout, start = 0, end = 1011):
-        decisionHeuristicFactory = lambda:  DummyBranchDecision()#RandomFalseBranchDecision()
-
         rules, numOfVars = load_dimacs('rules/sudoku_rules_9x9.txt')
         sudokus = load_sudokus('sudokus/1000_sudokus_9x9.txt')[start:end]
         data = []
@@ -94,7 +93,7 @@ class DiffConstraintsExperiment:
                 before = time.time()
                 solver = BasicDPLL(cnf,
                     numOfVars,
-                    decisionHeuristicFactory,
+                    self.decisionHeuristicFactory,
                     timeout,
                     5,
                     instanceMetrics
@@ -151,7 +150,9 @@ if __name__ == "__main__":
     timeout = args.timeout
     print("numOfConstraints={}, timeout={}, start={}, end={}".format(numOfContraints, timeout, start, end))
 
+    decisionHeuristicFactory = lambda:  DummyBranchDecision()#RandomFalseBranchDecision()
+
     before = time.time()
-    experiment = DiffConstraintsExperiment()
+    experiment = DiffConstraintsExperiment(decisionHeuristicFactory)
     experiment.run(numOfContraints, timeout, start, end)
     print("time={} seconds".format(time.time()-before))

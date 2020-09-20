e842b4000e580d245d97f1a75a562f2691e0980c
Andreas Sauter <andreas.sauter@student.uni-tuebingen.de>
2020-09-20 17:44:07 +0200

    analysis and more plots

--- analysis_ex2.py
+++ analysis_ex2.py
@@ -1,29 +1,47 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+from scipy.stats import kendalltau
+import seaborn as sns
+
+metrics = ['loop', 'backtrack']#, 'flip', 'unit']
 
 #load the dataset
 data = pd.read_csv('SingleCummulativeForEachConstraint_0_1011.csv')
-# delete rows where flip is bigger than 500. WARNING: tilts the data
-# data = data.drop(data.loc[data['flip'] > 500].index, axis=0)
 print(data.describe())
 
 #first, let's plot some stuff
 data_grouped_by_n_constr = data.groupby(['numOfConstraints'])
-means = data_grouped_by_n_constr.mean()
+medians = data_grouped_by_n_constr.median()
 
 #plot developement of means
 if False:
-    for metric in ['loop']:#, 'backtrack', 'flip', 'unit']:
-        plt.plot(means[metric], label='mean')
+    for metric in metrics:
+        plt.plot(medians[metric], label='mean')
         plt.ylabel(metric+'s')
         plt.xlabel('number of constraints')
         plt.legend()
         plt.show()
 
+#plt.hist(data[data['numOfConstraints']==15]['flip'], bins=100)
+#plt.show()
 # scatter plots
 if False:
-    for metric in ['loop', 'backtrack', 'flip', 'unit']:
-        plt.scatter(data['numOfConstraints'], data[metric])
+    for metric in metrics:
+        plt.plot('numOfConstraints', metric, data=data, linestyle='', marker='o', markersize=0.8)
         plt.ylabel(metric + 's')
         plt.xlabel('number of constraints')
         plt.show()
+
+# 2D density plots. Basically same as scatter plot but nicer to 'read'. But they don't manage to show anything.
+if False:
+    for metric in metrics:
+        plt.hist2d(data[data[metric]<3000]['numOfConstraints'], data[data[metric]<3000][metric], bins=[27, 70], cmap=plt.cm.Reds)
+        plt.ylim(0, 3000)
+        plt.colorbar()
+        plt.show()
+
+# test if the ranks of the metrics values are correlated with number of constraints with
+# kendall's tau
+for metric in metrics:
+    print('correlation for ' + metric)
+    print(kendalltau(data['numOfConstraints'], data[metric]))

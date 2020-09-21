c7feb2246b88faad1e62cde61c8b8364c19c66af
Andreas Sauter <andreas.sauter@student.uni-tuebingen.de>
2020-09-21 12:24:53 +0200

    support for averaging over data

--- analysis_ex2.py
+++ analysis_ex2.py
@@ -3,20 +3,29 @@ from matplotlib import pyplot as plt
 from scipy.stats import kendalltau
 import seaborn as sns
 
+# settings
 metrics = ['loop', 'backtrack', 'flip', 'unit']
+average_data_over_runs = True
 
 #load the dataset
-data = pd.read_csv('SingleCummulativeForEachConstraint_0_1011.csv')
-print(data.describe())
+data = pd.read_csv('results/combined_SingleCummulativeForEachConstraint_0_1011.csv')
 
 #first, let's plot some stuff
-data_grouped_by_n_constr = data.groupby(['numOfConstraints'])
+# not averaging over runs:
+if average_data_over_runs == False:
+    data_grouped_by_n_constr = data.groupby(['numOfConstraints'])
+
+#averaging over runs: comment next two lines if not desired
+if average_data_over_runs == True:
+    data_grouped_by_n_constr = data.groupby(['sudokuID', 'numOfConstraints']).mean().groupby(['numOfConstraints'])
+    data=data.groupby(['sudokuID', 'numOfConstraints']).mean().reset_index()
+
 medians = data_grouped_by_n_constr.median()
 
-#plot developement of means
+#plot developement of median
 if False:
     for metric in metrics:
-        plt.plot(medians[metric], label='mean')
+        plt.plot(medians[metric], label='median')
         plt.ylabel(metric+'s')
         plt.xlabel('number of constraints')
         plt.legend()
